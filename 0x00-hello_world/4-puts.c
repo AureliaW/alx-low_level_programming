2e0c8eacd8c443804380038a8d4df020c6184820
AureliaW <kangangi.aurelia@gmail.com>
2022-12-08 07:39:51 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Start of program
+* Return: 0
+*
+*
+*/
+
+int main(void)
+{
+	char Mystr[];
+
+	Mystr[] = ""Programming is like building a multilingual puzzle";
+	puts(Mystr);
+
+	return (0);
+}
