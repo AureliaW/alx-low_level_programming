19c77ded0fa34a505b7fd0ce5aa3e09a224718bc
AureliaW <kangangi.aurelia@gmail.com>
2022-12-09 14:06:15 -0800

    Print numbers to base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Print single digits of base 10
+* Return: 0 on success
+*
+*
+*
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i == 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
