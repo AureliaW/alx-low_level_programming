dc1441b536a717613af80965e200545331268234
AureliaW <kangangi.aurelia@gmail.com>
2022-12-09 14:29:52 -0800

    print alphabet in lower case

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Print alpahabet in reverse order
+*
+* return: 0 on success
+*
+*
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i = 'a'; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
