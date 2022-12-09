b389e719d3815570fc0e0b29765c15a761577993
AureliaW <kangangi.aurelia@gmail.com>
2022-12-09 14:56:26 -0800

    Printing both alphabet and numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Printing alphabets to f and numbers to 9
+*
+* Return: 0 on sucess
+*
+*/
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+
+	putchar('\n');
+
+	return (0);
+}
