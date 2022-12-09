b891f814179bc61d70d207da886ff2f5193dfa53
AureliaW <kangangi.aurelia@gmail.com>
2022-12-09 06:18:33 -0800

    Assign numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Print numbers
+* Return: 0 on sucess
+*
+*
+*/
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive", n);
+	if (n == 0)
+		printf("%d is zero", n);
+	if  (n < 0)
+		printf("%d is negative", n);
+	return (0);
+}
