ecb41f05d29fbbed8df0a04aad7ae606595bf7e3
AureliaW <kangangi.aurelia@gmail.com>
2022-12-14 05:13:21 -0800

    Print alphabet in lower case 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main"
+/**
+ * print_alphabet_x10 - Print alphabet in lower case
+ *
+ * Return: 0 on success
+ *
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	char j;
+
+	for (j = 1; j < 10; j++)
+	{
+		for (i = a; i <= z; i++)
+			_putchar(i);
+
+		_putchar('\n)
+
+	}
+}
