73cdde204db51ea11eb3904cbe877a40520114da
AureliaW <kangangi.aurelia@gmail.com>
2022-12-15 14:57:38 -0800

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ * Fizz_Buzz - displays pattern
+ *
+ * @void: no parameters
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 15 == 0)
+			printf("FizzBuzz");
+		else if (n % 5 == 0)
+		       printf("	Buzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%d", n);
+		if (n < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
