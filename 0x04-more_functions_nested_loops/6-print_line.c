c80c60a1a429bef7ad3eafce37c1945870376773
AureliaW <kangangi.aurelia@gmail.com>
2022-12-15 14:08:15 -0800

    Draw a line on terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straiht line on terminal
+ * @n: input integer
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+		_putchar('\n');
+}
