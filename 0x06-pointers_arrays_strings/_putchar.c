69172a63e876981c5c596d19bc323c8b4b7eda18
AureliaW <kangangi.aurelia@gmail.com>
2023-01-09 11:18:08 -0800

    putchar

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _putchar - prints a character to stdout
+ * @ch: argument
+ *
+ * Return: 0 (success)
+ */
+
+int _putchar(char ch)
+{
+	ch = 'A';
+
+	_puthar(ch);
+	return (0);
+}
